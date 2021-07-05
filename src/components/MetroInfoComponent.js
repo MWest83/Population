e3b6cfd3800929e651d2309b12e360699ae4557f
Michael West <michael.west8384@gmail.com>
2021-07-05 15:07:06 -0500

    MetroInfo Component Part 2

---
+++ src/components/MetroInfoComponent.js
@@ -0,0 +1,15 @@
+class MetroInfo extends Component {
+
+    render() {
+        if(this.props.metro) {
+        return (
+            <div className='row'>
+
+            </div>
+        );
+    }
+    return <div/>;
+    }
+}
+
+export default MetroInfo;
