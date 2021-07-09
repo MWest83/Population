5bce2606b8e21e99a54f7b16908c3a99a059ac27
Michael West <michael.west8384@gmail.com>
2021-07-09 09:50:47 -0500

    Reformat Data

---
+++ src/shared/suburb.js
@@ -0,0 +1,284 @@
+export const suburbs = [
+    {
+        id: 0,
+        city: 'Brooklyn',
+        image: 'brooklynSub.jpg',
+        population: 2559903,
+        description: "Brooklyn is the most populated borough in the New York City metro."
+    },
+    {
+        id: 1,
+        city: 'Queens',
+        image: 'queensSub.jpg',
+        population: 2253858,
+        description: "Queens is the 2nd most populated borough in the New York City metro."
+    },
+    {
+        id: 2,
+        city: 'Manhatten',
+        image: 'manhattenSub.jpg',
+        population: 1628706,
+        description: "Manhatten is the 3rd most populated borough in the New York City metro."
+    },
+    {
+        id: 3,
+        city: 'Bronx',
+        image: 'bronxSub.jpg',
+        population: 1418207,
+        description: "Bronx is the 4th most populated borough in the New York City metro."
+    },
+    {
+        id: 4,
+        city: 'Staten-Island',
+        image: 'statenislandSub.jpg',
+        population: 476143,
+        description: "Staten island is the 5th most populated borough in the New York City metro."
+    },  
+    {
+        id: 5,
+        city: 'Long Beach',
+        image: 'longbeachSub.jpg',
+        population: 465576,
+        description: "Long Beach is one of the most populated cities in the Los Angeles metro."
+    },
+    {
+        id: 6,
+        city: 'Anaheim',
+        image: 'anaheimSub.jpg',
+        population: 341361,
+        description: "Anaheim is one of the most populated cities in the Los Angeles metro."
+    },
+    {
+        id: 7,
+        city: 'Santa Ana',
+        image: 'santaanaSub.jpg',
+        population: 329427,
+        description: "Santa Ana is one of the most populated cities in the Los Angeles metro."
+    },
+    {
+        id: 8,
+        city: 'Irvine',
+        image: 'irvineSub.jpg',
+        population: 215529,
+        description: "Irvine is one of the most populated cities in the Los Angeles metro."
+    }, 
+    {
+        id: 9,
+        city: 'Auroura',
+        image: 'aurouraSub.jpg',
+        population: 198870,
+            description: "Auroura is one of the most populated cities in the Chicago metro."
+    },
+    {
+        id: 10,
+        city: 'Naperville',
+        image: 'napervilleSub.jpg',
+        population: 149196,
+        description: "Naperville is one of the most populated cities in the Chicago metro."
+    },
+    {
+        id: 11,
+        city: 'Joliet',
+        image: 'jolietSub.jpg',
+        population: 329427,
+        description: "Joliet is one of the most populated cities in the Chicago metro."
+    },
+    {
+        id: 12,
+        city: 'Elgin',
+        image: 'irvineSub.jpg',
+        population: 111401,
+        description: "Elgin is one of the most populated cities in the Chicago metro."
+    }, {
+        id: 13,
+        city: 'Fort Worth',
+        image: 'fortworthSub.jpg',
+        population: 909585,
+        description: "Fort Worth is one of the most populated cities in the DFW metro."
+    },
+    {
+        id: 14,
+        city: 'Arlington',
+        image: 'arlingtonSub.jpg',
+        population: 398854,
+        description: "Arlington is one of the most populated cities in the DFW metro."
+    },
+    {
+        id: 15,
+        city: 'Plano',
+        image: 'planoSub.jpg',
+        population: 287677,
+        description: "Plano is one of the most populated cities in the DFW metro."
+    },
+    {
+        id: 16,
+        city: 'Garland',
+        image: 'garlandSub.jpg',
+        population: 239928,
+        description: "Garland is one of the most populated cities in the DFW metro."
+    }, {
+        id: 17,
+        city: 'Pasadena',
+        image: 'pasadenaSub.jpg',
+        population: 154193,
+        description: "Pasadena is one of the most populated cities in the Houston metro."
+    },
+    {
+        id: 18,
+        city: 'Pearland',
+        image: 'pearlandSub.jpg',
+        population: 122149,
+        description: "Pearland is one of the most populated cities in the Houston metro."
+    },
+    {
+        id: 19,
+        city: 'Sugarland',
+        image: 'sugarlandSub.jpg',
+        population: 118600,
+        description: "Sugarland is one of the most populated cities in the Houston metro."
+    },
+    {
+        id: 20,
+        city: 'The Woodlands',
+        image: 'thewoodlandsSub.jpg',
+        population: 116278,
+        description: "The Woodlands is one of the most populated cities in the Houston metro."
+    },{
+        id: 21,
+        city: 'Arlington',
+        image: 'arlingtonvaSub.jpg',
+        population: 236842,
+        description: "Arlington is one of the most populated cities in the Washington DC metro."
+    },
+    {
+        id: 22,
+        city: 'Alexadria',
+        image: 'alexadriaSub.jpg',
+        population: 157613,
+        description: "Alexadria is one of the most populated cities in the Washington DC metro."
+    },
+    {
+        id: 23,
+        city: 'Silver Spring',
+        image: 'silverspringSub.jpg',
+        population: 81773,
+        description: "Silver Spring is one of the most populated cities in the Washington DC metro."
+    },
+    {
+        id: 24,
+        city: 'Gaithersburg',
+        image: 'gaithersburgSub.jpg',
+        population: 67742,
+        description: "Gaithersburg is one of the most populated cities in the Washington DC metro."
+    },{
+        id: 25,
+        city: 'Hialeah',
+        image: 'hialeahSub.jpg',
+        population: 238942,
+        description: "Hialeah is one of the most populated cities in the Miami metro."
+    },
+    {
+        id: 26,
+        city: 'Fort Lauderdale',
+        image: 'ftlauderdaleSub.jpg',
+        population: 182595,
+        description: "Fort Lauderdale is one of the most populated cities in the Miami metro."
+    },
+    {
+        id: 27,
+        city: 'Pembroke Pines',
+        image: 'pembrokepinesSub.jpg',
+        population: 172374,
+        description: "Pembroke Pines is one of the most populated cities in the Miami metro."
+    },
+    {
+        id: 28,
+        city: 'Hollywood',
+        image: 'hollywoodSub.jpg',
+        population: 154823,
+        description: "Hollywood is one of the most populated cities in the Miami metro."
+    },{
+        id: 29,
+        city: 'Reading',
+        image: 'readingSub.jpg',
+        population: 87575,
+        description: "Reading is one of the most populated cities in the Philadelphia metro."
+    },
+    {
+        id: 30,
+        city: 'Upper Darby',
+        image: 'upperdarbySub.jpg',
+        population: 82765,
+        description: "Upper Darby is one of the most populated cities in the Philadelphia metro."
+    },
+    {
+        id: 31,
+        city: 'Camden',
+        image: 'camdenSub.jpg',
+        population: 74420,
+        description: "Camden is one of the most populated cities in the Philadelphia metro."
+    },
+    {
+        id: 32,
+        city: 'Wilmington',
+        image: 'wilmingtonSub.jpg',
+        population: 71502,
+        description: "Wilmington is one of the most populated cities in the Philadelphia metro."
+    },
+    {
+        id: 33,
+        city: 'Sandy Springs',
+        image: 'sandyspringsSub.jpg',
+        population: 93853,
+        description: "Sandy Springs is one of the most populated cities in the Atlanta metro."
+    },
+    {
+        id: 34,
+        city: 'Roswell',
+        image: 'roswellSub.jpg',
+        population: 88346,
+        description: "Roswell is one of the most populated cities in the Atlanta metro."
+    },
+    {
+        id: 35,
+        city: 'Johns Creek',
+        image: 'johnscreekSub.jpg',
+        population: 76728,
+        description: "Johns Creek is one of the most populated cities in the Atlanta metro."
+    },
+    {
+        id: 36,
+        city: 'Alpharetta',
+        image: 'alpharettaSub.jpg',
+        population: 57551,
+        description: "Alpharetta is one of the most populated cities in the Philadelphia metro."
+    },{
+        id: 37,
+        city: 'Mesa',
+        image: 'mesaSub.jpg',
+        population: 518012,
+        description: "Mesa is one of the most populated cities in the Phoenix metro."
+    },
+    {
+        id: 38,
+        city: 'Chandler',
+        image: 'chandlerSub.jpg',
+        population: 261165,
+        description: "Chandler is one of the most populated cities in the Phoenix metro."
+    },
+    {
+        id: 39,
+        city: 'Scottsdale',
+        image: 'scottsdaleSub.jpg',
+        population: 258069,
+        description: "Scottsdale is one of the most populated cities in the Phoenix metro."
+    },
+    {
+        id: 40,
+        city: 'Gilbert',
+        image: 'gilbertSub.jpg',
+        population: 254114,
+        description: "Gilbert is one of the most populated cities in the Phoenix metro."
+    }
+
+]
