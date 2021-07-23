742a62da08b72d73342ed78e1c1c9dce794a2e51
Michael West <michael.west8384@gmail.com>
2021-07-23 16:42:47 -0500

    Worked on Header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,10 +7,11 @@ class Header extends Component {
     constructor(props) {
         super(props);
         
-        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
             isNavOpen: false
         };
+
+        this.toggleNav = this.toggleNav.bind(this);
     }
 
     toggleNav() {
@@ -25,43 +26,42 @@ class Header extends Component {
                 <Jumbotron fluid>
                     <div className="container">
                         <div className="row">
-                            <div classname="col">
-                                <h1>Top 10 most Populated</h1>
-                                <h2>Most populated cities</h2>
+                            <div className="col">
+                                <h1>Top 10 Most Populated in the United States</h1>
+                                <h2>Metro Areas, States, and Zip Codes</h2>
                             </div>
                         </div>
                     </div>
                 </Jumbotron>
                 <Navbar dark sticky="top" expand="md">
-                <div className="container">
-                    <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/peoplelogo.jpg" 
-                    height="30" width="30" alt="Population Logo" /></NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNav} />
-                    <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink classname="nav=link" to="/home">
-                                    <i className="fa fa-home fa-lg" /> Home
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink classname="nav=link" to="/directory">
-                                    <i className="fa fa-list fa-lg" /> 10 Most Populated Cites
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink classname="nav=link" to="/topstate">
-                                    <i className="fa fa-info fa-lg" />10 Most Populated States 
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink classname="nav=link" to="/zip">
-                                    <i className="fa fa-address-card fa-lg" /> 10 Most Populated Zip Codes
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </div>
+                    <div className="container">
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/popLogo.png" height="30" width="30" alt="Population Logo" /></NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                            <Collapse isOpen={this.state.isNavOpen} navbar>
+                                <Nav navbar>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/home">
+                                            <i className="fa fa-hand-lizard-o fa-lg" /> Home
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/directory">
+                                            <i className="fa fa-hand-peace-o fa-lg" /> 10 Most Populated Cites
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/topstate">
+                                            <i className="fa fa-hand-spock-o fa-lg" />10 Most Populated States 
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/zip">
+                                            <i className="fa fa-hand-scissors-o fa-lg" /> 10 Most Populated Zip Codes
+                                        </NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </Collapse>
+                        </div>
                 </Navbar>
             </React.Fragment>
         );
